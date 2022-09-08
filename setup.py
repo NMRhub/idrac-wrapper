140eaf76593d163223acfe28093edc4b2a2a64d5
Gerard Weatherby <gweatherby@uchc.edu>
2022-09-08 11:30:56 -0400

    More files

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.6
+from setuptools import setup
+with open("README.rst", "r") as fh:
+    readme_long_description = fh.read()
+
+setup(name='idrac-wrapper',
+      version='1.0',
+      description='iDRAC redfish library wrapper',
+      long_description_content_type='text/x-rst',
+      long_description = readme_long_description,
+      maintainer='Gerard Weatherby',
+      install_requires=['redfish'],
+      maintainer_email='gweatherby@uchc.edu',
+      packages=[
+          'idrac','scripts'
+      ],
+      entry_points={
+          'console_scripts':
+              [
+                  'idrac = scripts.manage:main',
+              ]
+      }
+)
